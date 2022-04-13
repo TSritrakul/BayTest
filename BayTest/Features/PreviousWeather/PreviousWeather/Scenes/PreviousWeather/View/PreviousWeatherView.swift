e0f9980459754d6ec62e3fd5ace668a5d19f2429
Thatchaphon Sritrakul <Thatchaphon.Sri@gmail.com>
2022-04-13 11:55:06 +0700

    Create PreviousWeatherView

---
+++ BayTest/Features/PreviousWeather/PreviousWeather/Scenes/PreviousWeather/View/PreviousWeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  PreviousWeatherView.swift
+//  PreviousWeather
+//
+//  Created by Thatchaphon Sritrakul on 13/4/2565 BE.
+//
+
+import SwiftUI
+
+struct PreviousWeatherView: View {
+    var body: some View {
+        Text("PreviousWeather")
+    }
+}
+
+struct PreviousWeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviousWeatherView()
+    }
+}
