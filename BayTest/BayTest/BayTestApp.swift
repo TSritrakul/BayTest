c501ace40d5119d39fa2eb9bb8a6956f4af7fc37
Thatchaphon Sritrakul <Thatchaphon.Sri@gmail.com>
2022-04-13 10:38:02 +0700

    Create New Project

---
+++ BayTest/BayTest/BayTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BayTestApp.swift
+//  BayTest
+//
+//  Created by Thatchaphon Sritrakul on 13/4/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct BayTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
